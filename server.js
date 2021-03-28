813f41a4487496c73632035437a470c56f2e82e8
Adam Forte <back.at.adam@gmail.com>
2021-03-28 16:49:55 -0400

    add first GET route

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const express = require("express");
-const app = express();
+
+const express = require("express"); //includes the express module
+const app = express(); //creates an express object named app.
+
+const mockUserData = [ //mock data for ther server to send to the user on a browser.
+    { name: 'Mark' },
+    {name:'Jill'}
+];
+
+app.get('/users', function (req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    });
+});
 
 app.listen(8000, function () {
     console.log("server is running...");
