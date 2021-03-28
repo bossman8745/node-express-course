64af454d681b8ca5b8527f007016e403b5d8c44a
Adam Forte <back.at.adam@gmail.com>
2021-03-28 17:22:36 -0400

    add login POST route

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require("express"); //includes the express module
 const app = express(); //creates an express object named app.
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 const mockUserData = [ //mock data for ther server to send to the user on a browser.
     { name: 'Mark' },
     {name:'Jill'}
@@ -22,6 +25,29 @@ app.get('/users/:id', function (req, res) {
         message: 'got one user!',
         user: req.params.id
     });
+});
+
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and usernam match!',
+            token: 'abcdefg'
+        });
+    }
+
+    else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        });
+    }
 });
 
 app.listen(8000, function () {
